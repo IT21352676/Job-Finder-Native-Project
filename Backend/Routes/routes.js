6b459ae489f2dceee8d2be6f0865f3a69fc0cb19
Hirusha Sasanka <100258430+IT21352676@users.noreply.github.com>
2025-06-30 16:14:17 +0530

    changes

--- Backend/Routes/routes.js
+++ Backend/Routes/routes.js
@@ -302,20 +302,14 @@ router.delete("/reject-job-cancel-request", (req, res) => {
 // AI features routes
 
 const generateJobRecommendations = require("../Functions/AIFeatures/jobRecommendation");
-const { sending } = require("../Functions/MessagingModule/Messaging");
-const { initSocket } = require("../socket");
+const generateUserRecommendations = require("../Functions/AIFeatures/UserRecommendation");
 
 router.post("/job-recommandation", (req, res) => {
   generateJobRecommendations(req, res);
 });
 
-// router.get("/job-details", (req, res) => {
-//   getJobsDetails(req, res);
-// });
-
-const io = initSocket(http.createServer(app));
-router.post("/messaging", (req, res) => {
-  sending(req, res);
+router.post("/user-recommandation", (req, res) => {
+  generateUserRecommendations(req, res);
 });
 
 module.exports = router;
